4695409ff3399efdc993fb247c4b215b40a22be3
Adrien Couque <adrien.couque@applidium.com>
2016-01-28 18:48:53 +0100

    Allow local configuration

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,12 @@ module Boxen
 
     # asset version, bump to expire everything
     config.assets.version = "1.0"
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
 
     # utf-8 errwhere in templates
     config.encoding = "utf-8"
